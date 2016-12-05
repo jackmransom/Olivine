089cf4d58d8f18854f81b84bee80aafb0272ef5e
Jack Ransom <jackmransom@gmail.com>
2016-12-05 23:26:37 +0000

    Last Python commit

--- pkmnsav.py
+++ pkmnsav.py
@@ -19,17 +19,28 @@ PKMN_GSC_BALL_POCKET_ITEM_LIST = 0x2465
 PKMN_GSC_PC_ITEM_LIST = 0x247F
 PKMN_C_CURR_PC_BOX_NO = 0x2700
 PKMN_C_JP_PC_BOX_NAMES = 0x26E5
+PKMN_C_TEAM_POKEMON_LIST = 0x2865
 PKMN_C_JP_TEAM_POKEMON_LIST = 0x281A
 PKMN_C_POKEDEX_OWNED = 0x2A27
 PKMN_C_POKEDEX_SEEN = 0x2A47
-PKMN_C_PLAYER_GENDER = 0x3E3D
+PKMN_C_GENDER = 0x3E3D
+PKMN_C_JP_PLAYER_GENDER = 0x8000
 
-PKMN_C_JP_PRIMARY_PART_START = 0x2009
+PKMN_C_PRIMARY_PART_START = 0x2009
+PKMN_C_PRIMARY_PART_END = 0x2B82
 PKMN_C_JP_PRIMARY_PART_END = 0x2B3A
-PKMN_C_JP_SECONDARY_PART_START = 0x7209
+
+PKMN_C_SECONDARY_PART_START = 0x1209
+PKMN_C_SECONDARY_PART_END = 0x1D82
+PKMN_C_JP_SECONDARY_PART_END = 0x7D3A
 PKMN_C_JP_SECONDARY_PART_END = 0x7D3A
 
-PKMN_C_JP_CHECKSUM_1 = 0x2D0D
+PKMN_C_PART_LENGTH = 0xB79
+PKMN_C_JP_PART_LENGTH = 0xB31
+
+
+PKMN_C_CHECKSUM_1 = 0x2D0D
+PKMN_C_CHECKSUM_2 = 0x1F0D
 PKMN_C_JP_CHECKSUM_2 = 0x7F0D
 
 PKMN_C_PLAYER_GENDER_MALE = 0x0
@@ -90,26 +101,33 @@ PKMN_JP_CHAR_TABLE = [ #Fill rest of this, 'f' is placeholder
   'f']
 
 def print_checksums(data):
-    sum = (data[PKMN_C_JP_CHECKSUM_1] << 8) | data[PKMN_C_JP_CHECKSUM_1+1]
-    sum_b = (data[PKMN_C_JP_CHECKSUM_2] << 8) | data[PKMN_C_JP_CHECKSUM_2+1]
+    sum = (data[PKMN_C_CHECKSUM_1] << 8) | data[PKMN_C_CHECKSUM_1+1]
     print(hex(sum))
-    print(hex(sum_b))
+    sum = (data[PKMN_C_CHECKSUM_2] << 8) | data[PKMN_C_CHECKSUM_2+1]
+    #sum = (data[PKMN_C_JP_CHECKSUM_1] << 8) | data[PKMN_C_JP_CHECKSUM_1+1]
+    print(hex(sum))
 
-def write_checksum(data):
+def write_checksums(data):
     sum = 0
-    for i in range(PKMN_C_JP_PRIMARY_PART_START, PKMN_C_JP_PRIMARY_PART_END+1):
+    for i in range(PKMN_C_PRIMARY_PART_START, PKMN_C_PRIMARY_PART_START+PKMN_C_PART_LENGTH):
         sum += data[i]
-    data[PKMN_C_JP_CHECKSUM_1] = sum >> 8
-    data[PKMN_C_JP_CHECKSUM_1+1] = sum & 0xFF
+    data[PKMN_C_CHECKSUM_1] = sum & 0xFF
+    data[PKMN_C_CHECKSUM_1 + 1 ] = sum >> 8
 
     sum = 0
-    for i in range(PKMN_C_JP_SECONDARY_PART_START, PKMN_C_JP_SECONDARY_PART_END+1):
+    for i in range(PKMN_C_SECONDARY_PART_START, PKMN_C_SECONDARY_PART_START+PKMN_C_PART_LENGTH):
         sum += data[i]
-    data[PKMN_C_JP_CHECKSUM_2] = sum >> 8
-    data[PKMN_C_JP_CHECKSUM_2+1] = sum & 0xFF
+    data[PKMN_C_CHECKSUM_2] = sum & 0xFF
+    data[PKMN_C_CHECKSUM_2 + 1] = sum >> 8
+
+
+def copy_data(data):
+    for i in range(0, PKMN_C_PART_LENGTH):
+        data[PKMN_C_SECONDARY_PART_START+i] = data[PKMN_C_PRIMARY_PART_START+i]
 
 def save_data_to_file(path, data):
-    write_checksum(data)
+    copy_data(data)
+    write_checksums(data)
     with open(path, 'wb') as f:
         f.write(data)
 
@@ -117,10 +135,12 @@ def print_jp_char(char):
     print(PKMN_JP_CHAR_TABLE[char], end = "")
 
 def foo(path, data):
-    #first_pokemon = PKMN_C_JP_TEAM_POKEMON_LIST+8
-    #attack_offset = first_pokemon + 0x26 # These are 2 byte values
-    #hp_offset = first_pokemon + 0x24
-    data[PKMN_C_PLAYER_GENDER] = PKMN_C_PLAYER_GENDER_MALE
+    first_pokemon = PKMN_C_TEAM_POKEMON_LIST+8
+    attack_offset = first_pokemon + 0x26 # These are 2 byte values
+    max_hp_offset = first_pokemon + 0x24
+    data[max_hp_offset] = 00 #nice
+    data[max_hp_offset+1] = 69 #nice
+    data[PKMN_C_GENDER] = PKMN_C_PLAYER_GENDER_MALE
     save_data_to_file(path, data)
 
 def main():
